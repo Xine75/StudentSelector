4ecbebcd7ffd471448d72aaf0cac87a1a633ee3e
Xine <cjdoza@gmail.com>
2021-05-28 15:06:21 -0500

    text file

--- Program.cs
+++ Program.cs
@@ -9,122 +9,120 @@ namespace StudentSelector
         static void Main(string[] args)
         {
 
-            string textFile = System.IO.File.ReadAllText("studentList.txt");
-            public List<string> Students1 = new List<string>();
-        Students1 = textFile.Split(", ");
+            List<string> Students1 = System.IO.File.ReadAllLines("studentList.txt").ToList();
 
-            List<string> Students1 = new List<string>()
-           {
-               "Chad Clark",
-                "Micaela Colpi",
-                "Sidney Crandall",
-                "Salma Crank",
-                "Carter Culkin",
-                "David Darden",
-                "Logan Demmy",
-                "Chris Douglass",
-                "Alex Dudley",
-                "Autumn Fotopoulos",
-                "Joel Gage",
-                "Bryson Goins",
-                "B.J. Golden",
-               "Jackson Goodman",
-                "Brandon Hill",
-                "Traye Johnson",
-                "Cody Jones",
-                "Bre Coach",
-                "Taryn Lytle",
-                "Ethan Mathis",
-                "Colten Mayberry",
-                "Meg McGregor",
-                "Laurel Morrison",
-                "Nick Patton",
-                "Hunter Preast",
-                "Jeremiah Schugt",
-                "Preston Shotts",
-                "Brandon Vinson",
-                "Dakota Upchurch"
-            };
 
-        CallOnStudents();
+            //     List<string> Students1 = new List<string>()
+            //    {
+            //        "Chad Clark",
+            //         "Micaela Colpi",
+            //         "Sidney Crandall",
+            //         "Salma Crank",
+            //         "Carter Culkin",
+            //         "David Darden",
+            //         "Logan Demmy",
+            //         "Chris Douglass",
+            //         "Alex Dudley",
+            //         "Autumn Fotopoulos",
+            //         "Joel Gage",
+            //         "Bryson Goins",
+            //         "B.J. Golden",
+            //        "Jackson Goodman",
+            //         "Brandon Hill",
+            //         "Traye Johnson",
+            //         "Cody Jones",
+            //         "Bre Coach",
+            //         "Taryn Lytle",
+            //         "Ethan Mathis",
+            //         "Colten Mayberry",
+            //         "Meg McGregor",
+            //         "Laurel Morrison",
+            //         "Nick Patton",
+            //         "Hunter Preast",
+            //         "Jeremiah Schugt",
+            //         "Preston Shotts",
+            //         "Brandon Vinson",
+            //         "Dakota Upchurch"
+            //     };
 
-        Again();
 
-        void CallOnStudents()
-        {
+            Again();
 
-            List<string> notCalledOn = new List<string>(Students1);
-
-            Console.WriteLine("You Have NOT Called On: ");
-            Console.WriteLine("------------------------------------------------------------");
-            // var notCalledOnString = String.Join(" ,", notCalledOn);
-            Console.WriteLine(String.Join(", ", notCalledOn));
-
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-
-            //create a list of students who have been called on
-            List<string> alreadyCalledOn = new List<string>();
-            while (alreadyCalledOn.Count < Students1.Count)
+            CallOnStudents();
+            void CallOnStudents()
             {
-                var random = new Random();
-                var candidate = random.Next(0, Students1.Count);
+                List<string> notCalledOn = new List<string>(Students1);
 
-                if (notCalledOn.Contains(Students1[candidate]))
+                Console.WriteLine("You Have NOT Called On: ");
+                Console.WriteLine("------------------------------------------------------------");
+                // var notCalledOnString = String.Join(" ,", notCalledOn);
+                Console.WriteLine(String.Join(", ", notCalledOn));
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+
+                //create a list of students who have been called on
+                List<string> alreadyCalledOn = new List<string>();
+                while (alreadyCalledOn.Count < Students1.Count)
                 {
-                    alreadyCalledOn.Add(Students1[candidate]);
-                    notCalledOn.Remove(Students1[candidate]);
+                    var random = new Random();
+                    var candidate = random.Next(0, Students1.Count);
+
+                    if (notCalledOn.Contains(Students1[candidate]))
+                    {
+                        alreadyCalledOn.Add(Students1[candidate]);
+                        notCalledOn.Remove(Students1[candidate]);
 
 
-                    var candidateString = String.Join(", ", Students1[candidate]);
-                    var notCalledOnString = String.Join(", ", notCalledOn);
-                    var alreadyCalledOnString = String.Join(", ", alreadyCalledOn);
+                        var candidateString = String.Join(", ", Students1[candidate]);
+                        var notCalledOnString = String.Join(", ", notCalledOn);
+                        var alreadyCalledOnString = String.Join(", ", alreadyCalledOn);
 
-                    Console.WriteLine("You Have NOT Called On: ");
-                    Console.WriteLine("------------------------------------------------------------");
-                    Console.WriteLine(notCalledOnString);
-                    Console.WriteLine();
+                        Console.WriteLine("You Have NOT Called On: ");
+                        Console.WriteLine("------------------------------------------------------------");
+                        Console.WriteLine(notCalledOnString);
+                        Console.WriteLine();
 
-                    Console.WriteLine("You've Already Called On: ");
-                    Console.WriteLine("------------------------------------------------------------");
-                    Console.WriteLine(alreadyCalledOnString);
-                    Console.WriteLine();
+                        Console.WriteLine("You've Already Called On: ");
+                        Console.WriteLine("------------------------------------------------------------");
+                        Console.WriteLine(alreadyCalledOnString);
+                        Console.WriteLine();
 
-                    Console.Write("UP NEXT: ");
-                    Console.WriteLine(candidateString);
-                    Console.WriteLine();
+                        Console.Write("UP NEXT: ");
+                        Console.WriteLine(candidateString);
+                        Console.WriteLine();
 
-                    Console.WriteLine("*Press any key to continue*");
+                        Console.WriteLine("*Press any key to continue*");
 
-                    Console.ReadLine();
-                    Console.Clear();
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                }
+            }
+
+            void Again()
+            {
+                Console.WriteLine("You've called on all the students. Start over? Y/N?");
+
+                string answer = Console.ReadLine().ToLower();
+
+                while (answer != "y" && answer != "n")
+                {
+                    Console.Write("Please select Y or N.");
+                    answer = Console.ReadLine().ToLower();
+                }
+
+                if (answer == "y")
+                {
+                    CallOnStudents();
+                }
+                else
+                {
+                    Console.WriteLine("Thank you and good-bye!");
+                    return;
                 }
             }
         }
-
-        void Again()
-        {
-            Console.WriteLine("You've called on all the students. Start over? Y/N?");
-
-            string answer = Console.ReadLine().ToLower();
-
-            while (answer != "y" && answer != "n")
-            {
-                Console.Write("Please select Y or N.");
-                answer = Console.ReadLine().ToLower();
-            }
-
-            if (answer == "y")
-            {
-                CallOnStudents();
-            }
-            else
-            {
-                Console.WriteLine("Thank you and good-bye!");
-                return;
-            }
-        }
     }
-}
 }
 
